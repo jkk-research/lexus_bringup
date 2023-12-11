ea60b9ba580e452878688f7d11d24eeda2cae6b3
szepilot <szepilot@gmail.com>
2023-12-11 16:04:19 +0100

    Mcap record options

--- etc/record_mcap1.sh
+++ etc/record_mcap1.sh
@@ -2,6 +2,8 @@
 
 ## ROS 2 rosbag recorder (MCAP format)
 
+cd "$(dirname "$0")"
+DIR1="$(pwd)"
 cd /mnt/bag
 mkdir -p $(date -I)
 cd $(date -I)
@@ -11,6 +13,4 @@ FILE1="$TEXT1$TIME1"
 FILE2="x_rosparam_dump_$TEXT1$TIME1.txt"
 PWD1="$(pwd)"
 echo "Writing to file: $PWD1/$FILE1"
-ros2 bag record -s mcap --all -o $FILE1 
-
-
+ros2 bag record -s mcap --all -o $FILE1 --max-cache-size 1048576000 --storage-config-file $DIR1/mcap_writer_options.yaml
