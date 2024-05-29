a0d4e161d6a302ba6712dd48630550a0d94cbb42
AnonymDavid <jodavid512@gmail.com>
2024-05-29 12:26:11 +0200

    Switched tf launchers to the new style arguments

--- launch/tf_static.launch.py
+++ launch/tf_static.launch.py
@@ -3,10 +3,6 @@ from launch_ros.actions import Node
 import os
 
 def generate_launch_description():
-
-    #pkg_name = 'lexus_bringup'
-    #pkg_dir = os.popen('/bin/bash -c "source /usr/share/colcon_cd/function/colcon_cd.sh && colcon_cd %s && pwd"' % pkg_name).read().strip()
-
     ns_vehicle = "lexus3"
 
     return LaunchDescription([
@@ -15,23 +11,54 @@ def generate_launch_description():
             executable='static_transform_publisher',
             name='gyor0_tf_publisher',
             output='screen',
-            ##  Old-style arguments are deprecated, parameters should be used, but this does not work TODO
-            arguments=['697237.0', '5285644.0', '0.0','0', '0', '0', '1','map','map_gyor_0'],
-            #parameters=[{'translation.x': 697237.0, 'translation.y': 5285644.0, 'translation.z': 0.0, 'rotation.x': 0.0, 'rotation.y': 0.0, 'rotation.z': 0.0, 'rotation.w': 1.0, 'frame_id': 'map', 'child_frame_id': 'map_gyor_0'}]
+            arguments=[
+                '--x',  '697237.0',
+                '--y',  '5285644.0',
+                '--z',  '0.0',
+                '--qx', '0.0',
+                '--qy', '0.0',
+                '--qz', '0.0',
+                '--qw', '1.0',
+
+                '--frame-id',      'map',
+                '--child-frame-id','map_gyor_0'
+            ],
         ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='zala0_tf_publisher',
             output='screen',
-            arguments=['639770.0', '5195040.0', '0.0','0', '0', '0', '1','map','map_zala_0'],
+            arguments=[
+                '--x',  '639770.0',
+                '--y',  '5195040.0',
+                '--z',  '0.0',
+                '--qx', '0.0',
+                '--qy', '0.0',
+                '--qz', '0.0',
+                '--qw', '1.0',
+
+                '--frame-id',       'map',
+                '--child-frame-id', 'map_zala_0'
+            ],
         ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='duro_gps_tf_publisher',
             output='screen',
-            arguments=['1.6', '0.0', '0.2','0', '0', '0', '1', ns_vehicle + '/' + 'base_link', ns_vehicle + '/' + 'duro_gps'],
+            arguments=[
+                '--x',  '1.6',
+                '--y',  '0.0',
+                '--z',  '0.2',
+                '--qx', '0.0',
+                '--qy', '0.0',
+                '--qz', '0.0',
+                '--qw', '1.0',
+
+                '--frame-id',       ns_vehicle + '/' + 'base_link',
+                '--child-frame-id', ns_vehicle + '/' + 'duro_gps'
+            ],
         ),
         Node(
             package='tf2_ros',
@@ -39,50 +66,121 @@ def generate_launch_description():
             name='base_gps_tf_publisher',
             output='screen',
             # https://raw.githubusercontent.com/wiki/szenergy/szenergy-public-resources/img/2022.L.01.svg
-            arguments=['-1.542', '-0.49', '-1.479', '0.0', '0', '0', ns_vehicle + '/' + 'gps', ns_vehicle + '/' + 'base_link'], # TODO
+            # TODO
+            arguments=[
+                '--x',     '-1.542',
+                '--y',     '-0.49',
+                '--z',     '-1.479',
+                '--yaw',   '0.0',
+                '--pitch', '0.0',
+                '--roll',  '0.0',
+
+                '--frame-id',       ns_vehicle + '/' + 'gps',
+                '--child-frame-id', ns_vehicle + '/' + 'base_link'
+            ],
         ),
-        # 1.5 deg to rad =  0.0261799
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='zed_camera_front_tf_publisher',
             output='screen',
-            arguments=['1.6', '0.0', '1.286','0', '0', '0', '1', ns_vehicle + '/' + 'base_link', ns_vehicle + '/' +'_left_camera_frame'],
+            arguments=[
+                '--x',  '1.6',
+                '--y',  '0.0',
+                '--z',  '1.286',
+                '--qx', '0.0',
+                '--qy', '0.0',
+                '--qz', '0.0',
+                '--qw', '1.0',
+
+                '--frame-id',       ns_vehicle + '/' + 'base_link',
+                '--child-frame-id', ns_vehicle + '/' +'_left_camera_frame'
+            ],
         ),
         Node(
             package='tf2_ros',
-            #namespace=ns_vehicle,
             executable='static_transform_publisher',
             name='duro_gps_imu_tf_publisher',
             output='screen',
-            arguments=['0.0', '0.0', '0.2','0', '0', '0', '1', ns_vehicle + '/' + 'base_link', ns_vehicle + '/' + 'duro_gps_imu'],
+            arguments=[
+                '--x',  '0.0',
+                '--y',  '0.0',
+                '--z',  '0.2',
+                '--qx', '0.0',
+                '--qy', '0.0',
+                '--qz', '0.0',
+                '--qw', '1.0',
+
+                '--frame-id',       ns_vehicle + '/' + 'base_link',
+                '--child-frame-id', ns_vehicle + '/' + 'duro_gps_imu'
+            ],
         ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='base_link_ground_link_publisher',
             output='screen',
-            arguments=['0.0', '0.0', '-0.37','0', '0', '0', '1', ns_vehicle + '/' + 'base_link', ns_vehicle + '/' + 'ground_link'],
+            arguments=[
+                '--x',  '0.0',
+                '--y',  '0.0',
+                '--z',  '-0.37',
+                '--qx', '0.0',
+                '--qy', '0.0',
+                '--qz', '0.0',
+                '--qw', '1.0',
+                
+                '--frame-id',       ns_vehicle + '/' + 'base_link',
+                '--child-frame-id', ns_vehicle + '/' + 'ground_link'],
         ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='left1_os_front_tf_publisher',
             output='screen',
-            arguments=['0.75', '0.5', '1.3','0', '0', '0', ns_vehicle + '/' + 'base_link', ns_vehicle + '/' + 'os_left_a'],
+            arguments=[
+                '--x',     '0.75',
+                '--y',     '0.5',
+                '--z',     '1.3',
+                '--yaw',   '0.0',
+                '--pitch', '0.0',
+                '--roll',  '0.0',
+
+                '--frame-id',       ns_vehicle + '/' + 'base_link',
+                '--child-frame-id', ns_vehicle + '/' + 'os_left_a'
+            ],
         ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='right1_os_front_tf_publisher',
             output='screen',
-            arguments=['1.53', '-0.5', '1.41','0', '0', '0', ns_vehicle + '/' + 'base_link', ns_vehicle + '/' + 'os_right_a'],
+            arguments=[
+                '--x',     '1.53',
+                '--y',     '-0.5',
+                '--z',     '1.41',
+                '--yaw',   '0.0',
+                '--pitch', '0.0',
+                '--roll',  '0.0',
+
+                '--frame-id',       ns_vehicle + '/' + 'base_link',
+                '--child-frame-id', ns_vehicle + '/' + 'os_right_a'
+            ],
         ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='center1_os_front_tf_publisher',
             output='screen',
-            arguments=['0.75', '0.0', '1.91','0', '0', '0', ns_vehicle + '/' + 'base_link', ns_vehicle + '/' + 'os_center_a'],
+            arguments=[
+                '--x',     '0.75',
+                '--y',     '0.0',
+                '--z',     '1.91',
+                '--yaw',   '0.0',
+                '--pitch', '0.0',
+                '--roll',  '0.0',
+
+                '--frame-id',       ns_vehicle + '/' + 'base_link',
+                '--child-frame-id', ns_vehicle + '/' + 'os_center_a'
+            ],
         )
     ])
