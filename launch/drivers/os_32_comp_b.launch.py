4678db5edb10442c8037352d18c4b45dc0c5c8c4
norbertmarko <norbertmarko92@gmail.com>
2024-07-12 15:19:42 +0200

    Opened intraproc branch.

---
+++ launch/drivers/os_32_comp_b.launch.py
@@ -0,0 +1,9 @@
+from launch import LaunchDescription
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():    
+    ld = LaunchDescription()
+
+    return ld
